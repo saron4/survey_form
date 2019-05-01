0fed937bdce6dfa1c145913d8eead1b3aa783810
saron4 <38431559+saron4@users.noreply.github.com>
2019-05-01 23:43:59 +0200

    initial commit

---
+++ styles.cs
@@ -0,0 +1,32 @@
+*{
+  box-sizing: border-box;
+}
+#survey-form{
+  border: 1px solid red;
+ margin: 5px;
+ display: flex;
+ 
+
+}
+.form-left{
+  width: 50%;
+  display: flex;
+  border: 1px solid BLACK;
+}
+.form-right{
+width: 50%;
+border: 1px solid green;
+}
+.a, .b{
+ 
+  padding: 10px;
+ margin: 5px;
+ 
+  
+}
+
+
+
+
+
+
